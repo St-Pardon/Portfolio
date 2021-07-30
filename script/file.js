393dd525a6bb57e1b81d7b81cc28e4e3832021d4
st-pardon <okc4pardon@gmail.com>
2021-07-30 18:07:54 +0100

    edit on js

--- script/file.js
+++ script/file.js
@@ -1,19 +1,23 @@
 const nav = document.querySelector(".navbar");
 const hamburger = document.querySelector(".fa-bars");
 const links = document.getElementsByClassName("links");
+const header = document.querySelector(".header")
 
 hamburger.addEventListener("click", () => {
     nav.classList.remove("close");
     nav.classList.toggle("open");
+    header.classList.remove("collapse")
     console.log("opened")
 });
 links[0].onclick = () => {
     nav.classList.remove("open");
     nav.classList.toggle("close");
+    header.classList.toggle("collapse")
     console.log("closed")
 };
 links[1].onclick = () => {
     nav.classList.remove("open");
     nav.classList.toggle("close");
+    header.classList.toggle("collapse")
     console.log("closed")
 }; 
