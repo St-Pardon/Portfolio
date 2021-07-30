ff5f371ff2c65b012926496c82df7ffcb0c93df2
st-pardon <okc4pardon@gmail.com>
2021-07-30 15:30:58 +0100

    changed the js

--- script/file.js
+++ script/file.js
@@ -1,13 +1,19 @@
-
 const nav = document.querySelector(".navbar");
 const hamburger = document.querySelector(".fa-bars");
-const links = document.querySelector(".links");
+const links = document.getElementsByClassName("links");
 
 hamburger.addEventListener("click", () => {
+    nav.classList.remove("close");
     nav.classList.toggle("open");
     console.log("opened")
 });
-links.addEventListener("click", () => {
+links[0].onclick = () => {
+    nav.classList.remove("open");
     nav.classList.toggle("close");
     console.log("closed")
-});
+};
+links[1].onclick = () => {
+    nav.classList.remove("open");
+    nav.classList.toggle("close");
+    console.log("closed")
+};
