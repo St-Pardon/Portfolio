2621952896e81cb2074ea0a8b05bc4de3023492a
st-pardon <okc4pardon@gmail.com>
2021-08-18 20:18:41 +0100

    major changes on UI

--- script/file.js
+++ script/file.js
@@ -1,23 +1,20 @@
 const nav = document.querySelector(".navbar");
 const hamburger = document.querySelector(".fa-bars");
 const links = document.getElementsByClassName("links");
-const header = document.querySelector(".header")
+const header = document.querySelector(".header");
 
 hamburger.addEventListener("click", () => {
     nav.classList.remove("close");
     nav.classList.toggle("open");
-    header.classList.remove("collapse")
-    console.log("opened")
+    header.classList.remove("collapse");
 });
 links[0].onclick = () => {
     nav.classList.remove("open");
     nav.classList.toggle("close");
-    header.classList.toggle("collapse")
-    console.log("closed")
+    header.classList.toggle("collapse");
 };
 links[1].onclick = () => {
     nav.classList.remove("open");
     nav.classList.toggle("close");
-    header.classList.toggle("collapse")
-    console.log("closed")
+    header.classList.toggle("collapse");
 }; 
