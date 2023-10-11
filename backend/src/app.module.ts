1b1b5514216bbff66d1910fd05bfe4ae9bd5ba1c
St Pardon <okc4pardon@gmail.com>
2023-10-11 14:21:57 +0100

    :bug: FIX: corrected prettier error and imported misssing module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProjectsModule } from './projects/projects.module';
+import { AppController } from './app/app.controller';
+import { AppService } from './app/app.service';
+import { ProjectsModule } from './modules/projects/projects.module';
 import { MONGO_URI } from './config/env.config';
+import { BlogPostsModule } from './modules/blog-posts/blog-posts.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(MONGO_URI), ProjectsModule],
+  imports: [
+    MongooseModule.forRoot(MONGO_URI),
+    ProjectsModule,
+    BlogPostsModule,
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
