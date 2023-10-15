db1128c57fb84178fb619fd186484e5d198a5b72
St Pardon <okc4pardon@gmail.com>
2023-10-15 14:54:27 +0100

    :hammer: UPDATE: restructured files and imports

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { LocalStrategy } from './local.strategy';
+import { LocalStrategy } from './strategy/local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/modules/user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -9,7 +9,7 @@ import { User, UserSchema } from 'src/modules/user/user.entity';
 import { UserService } from 'src/modules/user/user.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JWTSecret } from 'src/config/env.config';
-import { JwtStrategy } from './jwt.strategy';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
   imports: [
